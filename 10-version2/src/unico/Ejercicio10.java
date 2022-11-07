c890133ccc6e3f93d0338d9d522e58460bfa9081
Maria <maria_escribanoverde@gmail.com>
2022-11-07 16:15:50 +0100

    Version 2 - Final

--- 10-version2/src/unico/Ejercicio10.java
+++ 10-version2/src/unico/Ejercicio10.java
@@ -13,17 +13,28 @@ final int TOTAL_MIN = 1440;
 		int minuto;
 		int mintransc; 
 		int minrestante;
+		String formato;
+		int horasfaltan;
+		int minfaltan;
 		
-		System.out.print("A continuación deberá introducir una hora del día (formato 24 horas): ");
+		System.out.print("A continuación deberá introducir una hora del día (formato 12 horas): ");
 		hora = Integer.parseInt(teclado.nextLine());
+		System.out.print("¿AM / PM?");
+		formato = teclado.nextLine();
 		System.out.print("Ahora, introduzca los minutos: ");
 		minuto = Integer.parseInt(teclado.nextLine());
 		
+		if ( formato.equalsIgnoreCase("PM")) {
+			hora += 12;	
+		}
+		
 		mintransc = (hora * 60) + minuto;
 		minrestante = TOTAL_MIN - mintransc;
+		horasfaltan = (int) (minrestante / 60);
+		minfaltan = minrestante % 60;
 		
-		System.out.println("Desde las " + hora + ":" + minuto + " hasta la medianoche faltan " + minrestante + " minutos.");
-		
+		System.out.println("Desde las " + hora + ":" + minuto + " hasta la medianoche faltan " + horasfaltan + " horas y " + minfaltan +  " minutos.");
+		System.out.println("Es decir " + minrestante +  " minutos.");
 		}
+
 }
-	
